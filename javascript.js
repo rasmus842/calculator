3507119afef205a07d9e5f698768abb764110c56
rasmus842 <rasmuszalite@gmail.com>
2022-06-11 21:03:55 +0300

    Added mathematical operator functions in js

---
+++ javascript.js
@@ -0,0 +1,27 @@
+function operate(operator, a, b) {
+    return operator(a, b);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function modulo(a, b) {
+    return a % b;
+}
+
+function power(a, b) {
+    return a ** b;
+}
